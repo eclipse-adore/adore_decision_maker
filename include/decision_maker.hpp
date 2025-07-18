6a01703dd4cfb63d4ecad2298b44c86e89225d5d
sanath <sanath.konthala@dlr.de>
2025-07-18 14:04:20 +0200

    added time headway subscriber and documented the feature

--- include/decision_maker.hpp
+++ include/decision_maker.hpp
@@ -47,6 +47,7 @@
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 namespace adore
 {
@@ -110,6 +111,7 @@ private:
   rclcpp::Subscription<adore_ros2_msgs::msg::SafetyCorridor>::SharedPtr        subscriber_safety_corridor;
   rclcpp::Subscription<adore_ros2_msgs::msg::TrafficParticipantSet>::SharedPtr subscriber_traffic_participant_set;
   rclcpp::Subscription<adore_ros2_msgs::msg::TrafficParticipantSet>::SharedPtr subscriber_infrastructure_traffic_participants;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr                      subscriber_time_headway;
 
 
   // LATEST RECEIVED DATA
@@ -138,6 +140,7 @@ private:
   void suggested_trajectory_acceptance_callback( const std_msgs::msg::Bool& msg );
   void traffic_signals_callback( const adore_ros2_msgs::msg::TrafficSignals& msg );
   void traffic_participants_callback( const adore_ros2_msgs::msg::TrafficParticipantSet& msg );
+  void time_headway_callback( const std_msgs::msg::Float64& msg );
 
 
   // OTHER MEMBERS
